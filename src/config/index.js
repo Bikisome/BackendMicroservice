258370fe21ca02124d364b8c0bb0a5789cbfca82
Bikram Ghoshal <bikramghoshal6337@gmail.com>
2025-06-01 18:30:49 +0530

    till adding the sequllizing-cli

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,8 @@
+const logger = require('./loger-config');
+
 module.exports = {
     
 ServerConfig : require('./server-config'),
+Logger : require('./loger-config'),
+
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express  = require('express');
 
 
-const  { ServerConfig } = require('./config');
+const  { ServerConfig , Logger} = require('./config');
+
 const apiRoutes = require('./routes');
 
 
@@ -13,5 +14,5 @@ app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, ()=>{
     console.log(`Server is running on port : ${ServerConfig.PORT}`); 
-    console.log("working fine"); 
+    Logger.info("Server started successfully", {});
 })
